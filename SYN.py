7b6a5b493d0377487708959d762070869ec17645
rjscruggs <34218656+rjscruggs@users.noreply.github.com>
2017-12-06 17:54:45 -0700

    Update SYN.py

--- SYN.py
+++ SYN.py
@@ -1,39 +1,40 @@
+
 # Code by RJ Scruggs
 # 11/29/17
+# Last modified 12/6/17
 # Notes
 
 '''
-(Done) Create a an app to score keep for you
+Allow the app to save a scores from a previous game.(v2)
 
-Allow the app to save a scores from a previous game.
+Allow to import a scorecard in the case the score program is shut or messed up (v2)
 
-Allow to import a scorecard in the case the score program is shut or messed up
+Allow editing of players before game begins (v2)
 
-(Done) Create a rotation by removing an index of a list and appending it to the end,
-    so you dont need to do a bunch of other crap
+Allow rounds to be redone (v2)
 
-Allow editing of players before game begins
+Allow the score card to show stats like (how many time gone down, previous bids , etccc) (v2 or v3)
 
-Allow rounds to be redone
-
-Allow the score card to show stats like (how many time gone down, previous bids , etccc
-
-Allow a score check function
-
-(Done) Allow command to go down or prompt user how many rounds before hand
+Allow a score check function at any time (future version)
 
 Fix "you cant bid section"
 
-Create and print a FULL scoreboard that shows every round
+Create and print a FULL scoreboard that shows every round (v2)
 
-Fix numOfrounds if function
+Fix numOfrounds if function (v2)
+
+Implement logical bidding (Does this bid makes sense) (v2) 
 '''
+
+#### DISCLAIMER this is version 1 of Screw your neighbor! This is the bare minimum version which provides working functionality without expections and fancy features. This is a Work in Progress!
 
 import time
 import math
 
 
 # Welcoming text
+print("Version 1, This is a work in progress!")
+print("\n")
 print("Welcome to Screw Your Neighbor Score Keeper!")
 print("Enter the players names in order (clockwise).")
 print("Please start with the person dealing.")
@@ -52,7 +53,10 @@ done = False
 roundin = 0
 scoreCard = []
 yesOrno = []
-sumrounds = 1
+redo = False
+scoreround = 1
+firsthalf = True
+
 
 # Establish how many rounds
 def numOfrounds():
@@ -65,6 +69,12 @@ def numOfrounds():
         roundin = int(input("How many rounds would you like to play up to? "))
 
 
+
+def findWinner():
+    pass # figure this out later
+
+
+
 # Creates a rotation by moving the front player to the back of the list
 def rotation():
     global players
@@ -72,9 +82,11 @@ def rotation():
     players.append(players[0])
     players.remove(players[0])
 
+    
+
 # 1 round; declares who's deal it is; stores everyones bid; and adds points if you got your bid
 def rounds():
-    global roundNum, players, bids
+    global roundNum, players, bids, redo, scoreround
 
     bidSum = 0
     print("\n")
@@ -83,7 +95,8 @@ def rounds():
 
     print("It is " + players[0] + "'s deal!") # Declares who's deal it is and then moves the dealer to the back of the rotation.
     print("\n")
-    rotation()
+    if redo == False:
+        rotation()
 
     for i in range(len(players) -1 ): # asks all players EXCEPT last player what they want to bid
         bid = int(input("What would " + players[i] + " like to bid? "))
@@ -113,17 +126,7 @@ def rounds():
 
 
     trackScore()
-    
-    print("\n")
-    print("Scoreboard:")
 
-    print("-----------")
-    
-    for key in score.keys():
-        print(key, ": ", score[key], sep="")
-    print("\n")
-    
-##    bids.clear() 
 
 
 # finds the max amount of rounds you can play based on how many players
@@ -133,6 +136,17 @@ def findMaxround():
     return maxRound
 
 
+
+def lastround():
+    global roundNum, firsthalf
+
+    if roundNum == 1 and firsthalf == False:
+        return True
+    else:
+        return False
+
+
+    
 # Enroll the players into the current round
 def enroll():
     done = False
@@ -151,47 +165,87 @@ def enroll():
         score[(str(players[i]))] = 0
 
 
+
+
 # Adds the score after each round
 def trackScore():
     for i in range(len(players)):
         getBid = input("Did " + players[i] + " get what they bid? (y/n) ")
         if getBid == ("y") or getBid == ("Y") or getBid == ("Yes") or getBid == ("yes"):
             score[players[i]] = score[players[i]] + 10 + bids[i]
+        if getBid == ("n") or getBid == ("N") or getBid == ("No") or getBid == ("no"):
+            score[players[i]] = score[players[i]]
         yesOrno.append(getBid)
+
+        
 
 # Cycles the rounds by counting up and down and takes in what to do at the end of the round
 def roundCycle(x):
-    global roundin, roundNum
-
-    firsthalf = True # Establishs the counting of the round as the first half of the game
+    global roundin, roundNum, redo, scoreround, firsthalf
+    
+    
+     # Establishs the counting of the round as the first half of the game
     
     for i in range((x*2)-1):
         rounds()
-        task = input("What would you like to do next?\n\nType '1' to end the round and go to the next round.\nType '2' to redo the round.\nType '3' for a score check.\n")
-        if task == "1":
-            if roundin == roundNum:
-                firsthalf = False # When the peak round has been encountered, start counting down
-            if firsthalf == True: # First half
-                roundNum +=1
-            if firsthalf == False: # Second Half
-                roundNum -= 1
-            bids.clear()
-        if task == "2": # check to see if redo works on second half of game
-            x += 1
-            bids.clear()
-        if task == "3":
-            scorecard()
-            bids.clear()
-            if roundin == roundNum:
-                firsthalf = False # When the peak round has been encountered, start counting down
-            if firsthalf == True: # First half
-                roundNum +=1
-            if firsthalf == False: # Second Half
-                roundNum -= 1
-    sumrounds += 1
+        print("\n")
+        if not lastround():
+            
+            task = input("What would you like to do next?\n\nType '1' to end the round and go to the next round.\nType '2' for a score check.\n")
+            if task == "1":
+                if roundin == roundNum:
+                    firsthalf = False # When the peak round has been encountered, start counting down
+                if firsthalf == True: # First half
+                    roundNum +=1
+                if firsthalf == False: # Second Half
+                    roundNum -=1
+                if not roundNum == 1 and firsthalf == False:
+                    bids.clear()
+
+                    ## Come back to this function in v2
+    ##        if task == "2": # check to see if redo works on second half of game # Fix rotation for a redo round
+    ##            x += 1
+    ##            roundin += 1
+    ##            bids.clear()
+    ##            redo = True
+            if task == "2":
+                scorecard()
+                if not roundNum == 1 and firsthalf == False:
+                    bids.clear()
+                if roundin == roundNum:
+                    firsthalf = False # When the peak round has been encountered, start counting down
+                if firsthalf == True: # First half
+                    roundNum +=1
+                if firsthalf == False: # Second Half
+                    roundNum -= 1
+    
+            if task == "":
+                if roundin == roundNum:
+                    firsthalf = False # When the peak round has been encountered, start counting down
+                if firsthalf == True: # First half
+                    roundNum +=1
+                if firsthalf == False: # Second Half
+                    roundNum -=1
+                if not roundNum == 1 and firsthalf == False:
+                    bids.clear()
+
+            scoreround +=1
+
+        
+
+def playAgain():
+    global done
+
+    a = input("Would you like to play again? (y/n) ")
+    if a == "n":
+        done = True
+    else:
+        done = False
+    print("\n")
+                        
 def scorecard():
     
-    global roundin, roundNum, sumrounds
+    global roundin, roundNum, scoreround
     # Top line
 
     for a in range(len(players)):
@@ -233,9 +287,9 @@ def scorecard():
     print("+")
 
     
-    scoreround = 1
     
-    for a in range(sumrounds): # First half
+    
+    for a in range(1): # First half # Change range value for full scoreboard when ready
         
     # Beginning of Reoccuring section
 
@@ -329,13 +383,23 @@ def scorecard():
 
         print("+")
 
-        scoreround += 1
-##    sumrounds += 1
-
+        
 # Main
-enroll()
-numOfrounds()
-roundCycle(roundin)
+while done == False:
+    
+    enroll()
+    numOfrounds()
+    roundCycle(roundin)
+    scorecard()
+    playAgain()
+    ## Declare winner
+
+
+
+
+
+
+
 
 
 
